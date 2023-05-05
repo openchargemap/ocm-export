1ca8d83894ac13e478955c775b1184b2d4bea202
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-05-05 11:15:28 +0800

    Update - remove computed properties

--- refresh.bat
+++ refresh.bat
@@ -4,7 +4,7 @@ rmdir tmp /Q /S
 mkdir tmp
 mkdir data
 
-curl "https://api-01.openchargemap.io/v3/poi?client=ocm-data-export&maxresults=200000&compact=true&verbose=false&includecomments=true" --output ./tmp/poi.json
+curl "https://api-01.openchargemap.io/v3/poi?client=ocm-data-export&maxresults=300000&compact=true&verbose=false&includecomments=true&excludecomputed=true" --output ./tmp/poi.json
 curl "https://api-01.openchargemap.io/v3/referencedata?client=ocm-data-export" --output ./tmp/referencedata.json
 
 node ./tools/parse-all/index.js
